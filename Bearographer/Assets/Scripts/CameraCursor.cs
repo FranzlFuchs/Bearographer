83844b6f1ee2e5fe7d967f8a72340593618f3ad7
DHochenauer <60236110+FranzlFuchs@users.noreply.github.com>
2023-05-23 16:23:37 +0200

    collision with cursor & Bears

--- Bearographer/Assets/Scripts/CameraCursor.cs
+++ Bearographer/Assets/Scripts/CameraCursor.cs
@@ -4,21 +4,77 @@ using UnityEngine;
 
 public class CameraCursor : MonoBehaviour
 {
+
+
+
     [SerializeField]
     private Texture2D _cursorTexture;
+    
+    private float _cursorTexWidth;
+    
+    private float _cursorTexHeight;
+
+    private BoxCollider2D _collider;
+
+    public List<GameObject> _bearsInRange = new List<GameObject>();
 
     // Start is called before the first frame update
     void Start()
     {
-        Vector2 cursorOffset = new Vector2(_cursorTexture.width / 2, _cursorTexture.height / 2);
+        _cursorTexWidth = _cursorTexture.width;
+        _cursorTexHeight = _cursorTexture.height;
+
+        //Textur setzen
+        Vector2 cursorOffset = new Vector2(_cursorTexWidth / 2, _cursorTexHeight / 2);
         Cursor.SetCursor(_cursorTexture, cursorOffset, CursorMode.ForceSoftware);
+       
+       
+       //Collider an Texturgröße anpassen
+        _collider = GetComponent<BoxCollider2D>();
+
+        Vector2 screenPositionOR = new Vector2(Input.mousePosition.x + _cursorTexWidth / 2, Input.mousePosition.y + _cursorTexHeight / 2);
+        Vector2 screenPositionUL = new Vector2(Input.mousePosition.x - _cursorTexWidth / 2, Input.mousePosition.y - _cursorTexHeight / 2);
+
+        Vector2 worldPositionOR = Camera.main.ScreenToWorldPoint(screenPositionOR);
+        Vector2 worldPositionUL = Camera.main.ScreenToWorldPoint(screenPositionUL);
+        Vector2 S = new Vector2( worldPositionOR.x - worldPositionUL.x, worldPositionOR.y - worldPositionUL.y);
+        _collider.size = S;        
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        //gameObject Position an Maus ausrichten
         Vector2 screenPosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
         Vector2 worldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
         this.gameObject.transform.position = worldPosition;
+
+    
+
+    }
+
+    void OnCollisionExit2D(Collision2D coll)
+    {
+        if(coll.gameObject.CompareTag("Bear"))
+        {
+            Debug.Log("BEAR OUT");
+            _bearsInRange.Remove(coll.gameObject);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if(coll.gameObject.CompareTag("Bear"))
+        {
+            Debug.Log("BEAR IN");
+            _bearsInRange.Add(coll.gameObject);
+        }
+    }
+
+    void TakeShot()
+    {
+        
     }
 }
