144bb6ac43e2667633fc8be817b2aa59e3a1cb54
DHochenauer <60236110+FranzlFuchs@users.noreply.github.com>
2023-05-23 17:01:10 +0200

    Shot

--- Bearographer/Assets/Scripts/CameraCursor.cs
+++ Bearographer/Assets/Scripts/CameraCursor.cs
@@ -4,19 +4,19 @@ using UnityEngine;
 
 public class CameraCursor : MonoBehaviour
 {
-
-
-
     [SerializeField]
     private Texture2D _cursorTexture;
-    
+
+    [SerializeField]
+    private GameObject _bearPrefab;
+
     private float _cursorTexWidth;
-    
+
     private float _cursorTexHeight;
 
     private BoxCollider2D _collider;
 
-    public List<GameObject> _bearsInRange = new List<GameObject>();
+    private List<GameObject> _bearsInRange = new List<GameObject>();
 
     // Start is called before the first frame update
     void Start()
@@ -27,20 +27,26 @@ public class CameraCursor : MonoBehaviour
         //Textur setzen
         Vector2 cursorOffset = new Vector2(_cursorTexWidth / 2, _cursorTexHeight / 2);
         Cursor.SetCursor(_cursorTexture, cursorOffset, CursorMode.ForceSoftware);
-       
-       
-       //Collider an Texturgröße anpassen
+
+        //Collider an Texturgröße anpassen
         _collider = GetComponent<BoxCollider2D>();
 
-        Vector2 screenPositionOR = new Vector2(Input.mousePosition.x + _cursorTexWidth / 2, Input.mousePosition.y + _cursorTexHeight / 2);
-        Vector2 screenPositionUL = new Vector2(Input.mousePosition.x - _cursorTexWidth / 2, Input.mousePosition.y - _cursorTexHeight / 2);
+        Vector2 screenPositionOR = new Vector2(
+            Input.mousePosition.x + _cursorTexWidth / 2,
+            Input.mousePosition.y + _cursorTexHeight / 2
+        );
+        Vector2 screenPositionUL = new Vector2(
+            Input.mousePosition.x - _cursorTexWidth / 2,
+            Input.mousePosition.y - _cursorTexHeight / 2
+        );
 
         Vector2 worldPositionOR = Camera.main.ScreenToWorldPoint(screenPositionOR);
         Vector2 worldPositionUL = Camera.main.ScreenToWorldPoint(screenPositionUL);
-        Vector2 S = new Vector2( worldPositionOR.x - worldPositionUL.x, worldPositionOR.y - worldPositionUL.y);
-        _collider.size = S;        
-
-
+        Vector2 S = new Vector2(
+            worldPositionOR.x - worldPositionUL.x,
+            worldPositionOR.y - worldPositionUL.y
+        );
+        _collider.size = S;
     }
 
     // Update is called once per frame
@@ -51,30 +57,38 @@ public class CameraCursor : MonoBehaviour
         Vector2 worldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
         this.gameObject.transform.position = worldPosition;
 
-    
-
+        //Auslöser
+        if (Input.GetMouseButtonDown(0))
+        {
+            TakeShot();
+        }
     }
 
     void OnCollisionExit2D(Collision2D coll)
     {
-        if(coll.gameObject.CompareTag("Bear"))
+        if (coll.gameObject.CompareTag("Bear"))
         {
-            Debug.Log("BEAR OUT");
+            Debug.Log("OUT");
             _bearsInRange.Remove(coll.gameObject);
         }
     }
 
     void OnCollisionEnter2D(Collision2D coll)
     {
-        if(coll.gameObject.CompareTag("Bear"))
+        if (coll.gameObject.CompareTag("Bear"))
         {
-            Debug.Log("BEAR IN");
+            Debug.Log("IN");
             _bearsInRange.Add(coll.gameObject);
         }
     }
 
     void TakeShot()
     {
-        
+        int index = _bearsInRange.Count;
+        for (int i = 0; i < index; i++)
+        {
+            //_bearsInRange[0].SetActive(false);
+            Destroy(_bearsInRange[0]);
+        }
     }
 }
---
+++ Bearographer/Assets/Scripts/IBear.cs
@@ -0,0 +1,5 @@
+
+public interface IBear 
+{
+    public void GetPhotographed();
+}
